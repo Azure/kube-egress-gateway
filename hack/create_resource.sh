218d5867517adb32a58bcce42f7f3f32005ec44a
Wantong Jiang <wantjian@microsoft.com>
2022-09-12 08:32:46 +0000

    Add webhook creation to script

--- hack/create_resource.sh
+++ hack/create_resource.sh
@@ -1,14 +1,20 @@
 #/bin/bash
 
+if [ "$1" != "webhook" ] && [ "$1" != "api" ]; then
+    echo "Unrecognized first argument. Must be <api/webhook>, \"$1\" provided."
+    exit 1
+fi
+
 workdir=$(pwd)
 echo "workdir: $workdir"
 
 make kubebuilder
 cp cmd/kube-egress-gateway-controller/cmd/root.go cmd/kube-egress-gateway-controller/cmd/root.go.bak
 cp cmd/kube-egress-gateway-controller/cmd/root.go.bak main.go
-./bin/kubebuilder create api $@
+./bin/kubebuilder create $@
 if [ $? -ne 0 ]; then
-    echo "kubebuilder create api failed"
+    echo "kubebuilder create $1 failed"
+    rm -f cmd/kube-egress-gateway-controller/cmd/root.go.bak main.go
     exit 1
 fi
 
