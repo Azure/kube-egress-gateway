0e61ff444c100d3161b61227d4f5426f7b4419e3
Wantong Jiang <wantjian@microsoft.com>
2023-02-13 07:16:08 +0000

    implement sgw webhook

--- api/v1alpha1/staticgatewayconfiguration_webhook.go
+++ api/v1alpha1/staticgatewayconfiguration_webhook.go
@@ -25,7 +25,12 @@
 package v1alpha1
 
 import (
+	"fmt"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -40,8 +45,6 @@ func (r *StaticGatewayConfiguration) SetupWebhookWithManager(mgr ctrl.Manager) e
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 //+kubebuilder:webhook:path=/mutate-egressgateway-kubernetes-azure-com-v1alpha1-staticgatewayconfiguration,mutating=true,failurePolicy=fail,sideEffects=None,groups=egressgateway.kubernetes.azure.com,resources=staticgatewayconfigurations,verbs=create;update,versions=v1alpha1,name=mstaticgatewayconfiguration.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &StaticGatewayConfiguration{}
@@ -49,8 +52,6 @@ var _ webhook.Defaulter = &StaticGatewayConfiguration{}
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (r *StaticGatewayConfiguration) Default() {
 	staticgatewayconfigurationlog.Info("default", "name", r.Name)
-
-	// TODO(user): fill in your defaulting logic.
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
@@ -61,23 +62,66 @@ var _ webhook.Validator = &StaticGatewayConfiguration{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *StaticGatewayConfiguration) ValidateCreate() error {
 	staticgatewayconfigurationlog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return r.validateSGC()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *StaticGatewayConfiguration) ValidateUpdate(old runtime.Object) error {
 	staticgatewayconfigurationlog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return r.validateSGC()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *StaticGatewayConfiguration) ValidateDelete() error {
 	staticgatewayconfigurationlog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
+	// no need to validate delete at this moment
 	return nil
+}
+
+func (r *StaticGatewayConfiguration) validateSGC() error {
+	// need to validate either GatewayNodepoolName or GatewayVMSSProfile is provided, but not both
+	var allErrs field.ErrorList
+
+	if r.Spec.GatewayNodepoolName == "" && r.vmssProfileIsEmpty() {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("spec").Child("gatewaynodepoolname"),
+			fmt.Sprintf("GatewayNodepoolName: %s, GatewayVMSSProfile: %#v", r.Spec.GatewayNodepoolName, r.Spec.GatewayVMSSProfile),
+			"Either GatewayNodepoolName or GatewayVMSSProfile must be provided"))
+	}
+
+	if r.Spec.GatewayNodepoolName != "" && !r.vmssProfileIsEmpty() {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("spec").Child("gatewaynodepoolname"),
+			fmt.Sprintf("GatewayNodepoolName: %s, GatewayVMSSProfile: %#v", r.Spec.GatewayNodepoolName, r.Spec.GatewayVMSSProfile),
+			"Only one of GatewayNodepoolName and GatewayVMSSProfile should be provided"))
+	}
+
+	if !r.vmssProfileIsEmpty() {
+		if r.Spec.GatewayVMSSProfile.VMSSResourceGroup == "" {
+			allErrs = append(allErrs, field.Invalid(field.NewPath("spec").Child("gatewayvmssprofile").Child("vmssresourcegroup"),
+				r.Spec.GatewayVMSSProfile.VMSSResourceGroup,
+				"Gateway vmss resource group is empty"))
+		}
+		if r.Spec.GatewayVMSSProfile.VMSSName == "" {
+			allErrs = append(allErrs, field.Invalid(field.NewPath("spec").Child("gatewayvmssprofile").Child("vmssname"),
+				r.Spec.GatewayVMSSProfile.VMSSName,
+				"Gateway vmss name is empty"))
+		}
+		if r.Spec.GatewayVMSSProfile.PublicIpPrefixSize < 0 || r.Spec.GatewayVMSSProfile.PublicIpPrefixSize > 31 {
+			allErrs = append(allErrs, field.Invalid(field.NewPath("spec").Child("gatewayvmssprofile").Child("publicipprefixsize"),
+				r.Spec.GatewayVMSSProfile.PublicIpPrefixSize,
+				"Gateway vmss public ip prefix size should be between 0 and 31 inclusively"))
+		}
+	}
+
+	if len(allErrs) == 0 {
+		return nil
+	}
+	return apierrors.NewInvalid(
+		schema.GroupKind{Group: "egressgateway.kubernetes.azure.com", Kind: "StaticGatewayConfiguration"},
+		r.Name, allErrs)
+}
+
+func (r *StaticGatewayConfiguration) vmssProfileIsEmpty() bool {
+	return r.Spec.GatewayVMSSProfile.VMSSResourceGroup == "" &&
+		r.Spec.GatewayVMSSProfile.VMSSName == "" &&
+		r.Spec.GatewayVMSSProfile.PublicIpPrefixSize == 0
 }
