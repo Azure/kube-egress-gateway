eb31c18c3c3690d5d6b4f018d71b7bc5cc786989
moreOver0 <398289931@qq.com>
2025-04-22 20:30:24 +0800

    fix ginkgo timeout setting

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -29,8 +29,6 @@ var (
 )
 
 var _ = Describe("Test staticGatewayConfiguration deployment", func() {
-	SetDefaultTimeout(115 * time.Minute)
-
 	// use controller-runtime client to manage cr
 	var k8sClient client.Client
 	// there is no easy way to get pod log with controller-runtime client, use client-go client instead
